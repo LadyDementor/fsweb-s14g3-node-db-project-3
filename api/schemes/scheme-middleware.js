be435af8cd86f72ef8f41edfd43e2345d66035f0
LadyDementor <125830907+LadyDementor@users.noreply.github.com>
2023-05-31 14:20:38 +0300

    Comparing changes

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -1,18 +1,30 @@
+const schemeModel = require("./scheme-model");
+
 /*
   Eğer `scheme_id` veritabanında yoksa:
-
   durum 404
   {
     "message": "scheme_id <gerçek id> id li şema bulunamadı"
   }
 */
-const checkSchemeId = (req, res, next) => {
 
+
+const checkSchemeId = async (req, res, next) => {
+  try {
+    const isExistScheme = await schemeModel.findById(req.params.scheme_id);
+    if(!isExistScheme){
+      res.status(404).json({message:`scheme_id ${req.params.scheme_id} id li şema bulunamadı`});
+    }else{
+      req.currentScheme=isExistScheme;
+      next();
+    }
+  } catch (error) {
+    next(error);
+  }
 }
 
 /*
   Eğer `scheme_name` yoksa, boş string ya da string değil:
-
   durum 400
   {
     "message": "Geçersiz scheme_name"
@@ -20,12 +32,21 @@ const checkSchemeId = (req, res, next) => {
 */
 const validateScheme = (req, res, next) => {
 
+  try {
+    const {scheme_name} = req.body;
+    if(!scheme_name || typeof(scheme_name)!="string"){
+      res.status(400).json({message:"Geçersiz scheme_name"});
+    }else{
+      next();
+    }
+  } catch (error) {
+    next(error);
+  }
 }
 
 /*
   Eğer `instructions` yoksa, boş string yada string değilse, ya da
   eğer `step_number` sayı değilse ya da birden küçükse:
-
   durum 400
   {
     "message": "Hatalı step"
@@ -33,6 +54,16 @@ const validateScheme = (req, res, next) => {
 */
 const validateStep = (req, res, next) => {
 
+  try {
+    const {instructions,step_number} = req.body;
+    if(!instructions || typeof(step_number)!="number" || step_number<1){
+      res.status(400).json({"message": "Hatalı step"});
+    }else{
+      next()
+    }
+  } catch (error) {
+    next(error);
+  }
 }
 
 module.exports = {
